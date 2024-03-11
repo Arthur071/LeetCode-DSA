aa27592b6db21e0c531ec7c30001a185a623e510
Shadan ijmal <shadanijmal999@gmail.com>
2024-03-11 20:22:05 +0530

    today's problem

--- LeetCode Top Interview 150/Merge_sorted_array.cpp
+++ LeetCode Top Interview 150/Merge_sorted_array.cpp
@@ -1,9 +1,11 @@
-class Solution {
+class Solution
+{
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1;      // nums1's index (the actual nums)
-        int j = n - 1;      // nums2's index
-        int k = m + n - 1;  // nums1's index (the next filled position)
+  void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
+  {
+    int i = m - 1;     // nums1's index (the actual nums)
+    int j = n - 1;     // nums2's index
+    int k = m + n - 1; // nums1's index (the next filled position)
 
     while (j >= 0)
       if (i >= 0 && nums1[i] > nums2[j])
@@ -12,5 +14,3 @@ public:
         nums1[k--] = nums2[j--];
   }
 };
-        
-    
